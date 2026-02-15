2c11f648bebebdee3a66220efef9a2955dfd8869
Joel Sikström <joel.sikstrom@oracle.com>
2026-02-15 13:27:33 +0100

    WIP ArrayProperties flags

--- src/hotspot/share/ci/ciReplay.cpp
+++ src/hotspot/share/ci/ciReplay.cpp
@@ -890,19 +890,13 @@ class CompileReplay : public StackObj {
   }
 
   ObjArrayKlass* create_concrete_object_array_klass(ObjArrayKlass* obj_array_klass, TRAPS) {
-    ArrayKlass::ArrayProperties array_properties =
-    static_cast<ArrayKlass::ArrayProperties>(parse_int("array_properties"));
+    ArrayProperties array_properties = ArrayProperties(checked_cast<array_properties_flags_t>(parse_int("array_properties")));
     if (!Arguments::is_valhalla_enabled()) {
       // Ignore array properties.
       return obj_array_klass;
     }
 
-    if (array_properties != ArrayKlass::DEFAULT &&
-        array_properties != ArrayKlass::NULL_RESTRICTED &&
-        array_properties != ArrayKlass::NON_ATOMIC &&
-        array_properties != (ArrayKlass::NULL_RESTRICTED | ArrayKlass::NON_ATOMIC)) {
-      guarantee(false, "invalid array_properties: %d", array_properties);
-    }
+    guarantee(!array_properties.is_invalid(), "invalid array_properties: %d", array_properties.value());
 
     return obj_array_klass->klass_with_properties(array_properties, THREAD);
   }
---
+++ src/hotspot/share/oops/arrayPropertiesFlags.hpp
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) 2026, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ *
+ */
+
+#ifndef SHARE_OOPS_ARRAYPROPERTIESFLAGS_HPP
+#define SHARE_OOPS_ARRAYPROPERTIESFLAGS_HPP
+
+#include "utilities/globalDefinitions.hpp"
+#include "utilities/ostream.hpp"
+
+using array_properties_flags_t = u1;
+
+class ArrayProperties {
+ public:
+#define ARRAY_PROPERTIES_FLAGS_DO(flag)  \
+    flag(is_null_restricted    , 1 << 0) \
+    flag(is_non_atomic         , 1 << 1) \
+    flag(is_invalid            , 1 << 2) \
+    /* end of list */
+
+#define ARRAY_PROPERTIES_FLAGS_ENUM_NAME(name, value)    _misc_##name = value,
+  enum {
+    ARRAY_PROPERTIES_FLAGS_DO(ARRAY_PROPERTIES_FLAGS_ENUM_NAME)
+  };
+#undef ARRAY_PROPERTIES_FLAGS_ENUM_NAME
+
+  array_properties_flags_t _flags;
+
+ public:
+  ArrayProperties() : _flags(0) {}
+  ArrayProperties(array_properties_flags_t flags) : _flags(flags) {}
+
+  array_properties_flags_t value() const { return _flags; }
+
+  // Create getters and setters for the flag values.
+#define ARRAY_PROPERTIES_FLAGS_GET_SET(name, ignore)          \
+  bool name() const { return (_flags & _misc_##name) != 0; } \
+  void set_##name(bool b) {         \
+    assert(!name(), "set once");    \
+    if (b) _flags |= _misc_##name; \
+  }
+  ARRAY_PROPERTIES_FLAGS_DO(ARRAY_PROPERTIES_FLAGS_GET_SET)
+#undef ARRAY_PROPERTIES_FLAGS_GET_SET
+
+  const char* as_string() {
+    // Caller must have set a ResourceMark
+    stringStream ss;
+    if (is_invalid()) {
+      return "INVALID";
+    } else {
+      ss.print("%s", (is_null_restricted() != 0) ? "NULL_RESTRICTED " : "NULLABLE ");
+      ss.print("%s", (is_non_atomic() != 0) ? "NON_ATOMIC " : "ATOMIC ");
+    }
+    return ss.as_string();
+  }
+};
+
+inline bool operator==(ArrayProperties a, ArrayProperties b) {
+  return a._flags == b._flags;
+}
+
+#endif // SHARE_OOPS_ARRAYPROPERTIESFLAGS_HPP
